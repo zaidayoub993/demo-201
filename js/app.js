28854b9d932b7ec9eaa8fe18619c589136ccdfe6
zaidayoub993 <zsayoub@gmail.com>
2020-09-09 00:18:39 +0300

    not finished

--- js/app.js
+++ js/app.js
@@ -1,30 +1,104 @@
-'use strict'
-var num = 1;
-console.log(typeof num);
-var str = 'zaid';
-console.log(typeof str);
-var bool = true;
-console.log(typeof bool);
-// // if(condition){
-// //     do statement 
-// // }else{
-//     do another statemant 
+// 'use strict'
+// var num = 1;
+// console.log(typeof num);
+// var str = 'zaid';
+// console.log(typeof str);
+// var bool = true;
+// console.log(typeof bool);
+// // // if(condition){
+// // //     do statement 
+// // // }else{
+// //     do another statemant 
+// // }
+// var age = 28;
+
+// if (age > 27) {
+//     alert('hi');
 // }
-var age = 28;
 
-if (age > 27) {
-    alert('hi');
+// else {
+//     console.log('hello');
+// }
+// var isold = true;
+// var isfat = false;
+// if (isold && isfat) {
+//     console.log('hi fat old')
+// } else if (isold || isfat) {
+//     console.log('hi fat or old ');
+// } else {
+//     console.log('nooo');
+// }
+// var color = prompt ('what is fav color ?');
+// switch (color) {
+//     case 'red':
+//         console.log('red')
+//         break;
+//        case 'blue': 
+//             console.log('blue')
+//         break;
+//     default:
+//         console.log('black');
+//         break;
+// }
+'use strict'
+var name = prompt('do you know my name ?');
+var res = name.toUpperCase();
+switch (name) {
+    case 'yes':
+        break;
+    case 'no':
+        break;
+    default:
+        break;
 }
-
-else {
-    console.log('hello');
+var name = prompt('do you like coding ?');
+var res = name.toUpperCase();
+switch (name) {
+    case 'yes':
+        break;
+    case 'no':
+        break;
+    default:
+        break;
 }
-var isold = true;
-var isfat = false;
-if (isold && isfat) {
-    console.log('hi fat old')
-} else if (isold || isfat) {
-    console.log('hi fat or old ');
-} else {
-    console.log('nooo');
+var name = prompt('do you like pasta  ?');
+var res = name.toUpperCase();
+switch (name) {
+    case 'yes':
+        break;
+    case 'no':
+        break;
+    default:
+        break;
 }
+var name = prompt('do you like memes ?');
+var res = name.toUpperCase();
+switch (name) {
+    case 'yes':
+        break;
+    case 'no':
+        break;
+    default:
+        break;
+}
+var name = prompt('are you alive  ?');
+var res = name.toUpperCase();
+switch (name) {
+    case 'yes':
+        break;
+    case 'no':
+        break;
+    default:
+        break;
+}
+// function myFunction() {
+//     var str = "Hello World!";
+//     var res = str.toUpperCase();
+//     document.getElementById("demo").innerHTML = res;
+// }
+function func() { 
+    var str = 'name'; 
+    var string = str.toUpperCase(); 
+    document.write(string); 
+} 
+func(); 
