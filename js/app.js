66d6de37831b5c26a10dde9ad8cf8da94433498a
zaidayoub993 <zsayoub@gmail.com>
2020-09-12 09:50:11 +0300

    new css lab05b

--- js/app.js
+++ js/app.js
@@ -1,249 +1,249 @@
-// // 'use strict'
-// // var num = 1;
-// // console.log(typeof num);
-// // var str = 'zaid';
-// // console.log(typeof str);
-// // var bool = true;
-// // console.log(typeof bool);
-// // // // if(condition){
-// // // //     do statement 
-// // // // }else{
-// // //     do another statemant 
+// // // 'use strict'
+// // // var num = 1;
+// // // console.log(typeof num);
+// // // var str = 'zaid';
+// // // console.log(typeof str);
+// // // var bool = true;
+// // // console.log(typeof bool);
+// // // // // if(condition){
+// // // // //     do statement 
+// // // // // }else{
+// // // //     do another statemant 
+// // // // }
+// // // var age = 28;
+
+// // // if (age > 27) {
+// // //     alert('hi');
 // // // }
-// // var age = 28;
 
-// // if (age > 27) {
-// //     alert('hi');
-// // }
-
-// // else {
-// //     console.log('hello');
-// // }
-// // var isold = true;
-// // var isfat = false;
-// // if (isold && isfat) {
-// //     console.log('hi fat old')
-// // } else if (isold || isfat) {
-// //     console.log('hi fat or old ');
-// // } else {
-// //     console.log('nooo');
-// // }
-// // var color = prompt ('what is fav color ?');
-// // switch (color) {
-// //     case 'red':
-// //         console.log('red')
+// // // else {
+// // //     console.log('hello');
+// // // }
+// // // var isold = true;
+// // // var isfat = false;
+// // // if (isold && isfat) {
+// // //     console.log('hi fat old')
+// // // } else if (isold || isfat) {
+// // //     console.log('hi fat or old ');
+// // // } else {
+// // //     console.log('nooo');
+// // // }
+// // // var color = prompt ('what is fav color ?');
+// // // switch (color) {
+// // //     case 'red':
+// // //         console.log('red')
+// // //         break;
+// // //        case 'blue': 
+// // //             console.log('blue')
+// // //         break;
+// // //     default:
+// // //         console.log('black');
+// // //         break;
+// // // }
+// // 'use strict'
+// // var name = prompt('do you know my name ?');
+// // var res = name.toUpperCase();
+// // switch (name) {
+// //     case 'yes':
 // //         break;
-// //        case 'blue': 
-// //             console.log('blue')
+// //     case 'no':
 // //         break;
 // //     default:
-// //         console.log('black');
 // //         break;
 // // }
-// 'use strict'
-// var name = prompt('do you know my name ?');
-// var res = name.toUpperCase();
-// switch (name) {
-//     case 'yes':
-//         break;
-//     case 'no':
-//         break;
-//     default:
-//         break;
-// }
-// var name = prompt('do you like coding ?');
-// var res = name.toUpperCase();
-// switch (name) {
-//     case 'yes':
-//         break;
-//     case 'no':
-//         break;
-//     default:
-//         break;
-// }
-// var name = prompt('do you like pasta  ?');
-// var res = name.toUpperCase();
-// switch (name) {
-//     case 'yes':
-//         break;
-//     case 'no':
-//         break;
-//     default:
-//         break;
-// }
-// var name = prompt('do you like memes ?');
-// var res = name.toUpperCase();
-// switch (name) {
-//     case 'yes':
-//         break;
-//     case 'no':
-//         break;
-//     default:
-//         break;
-// }
-// var name = prompt('are you alive  ?');
-// var res = name.toUpperCase();
-// switch (name) {
-//     case 'yes':
-//         break;
-//     case 'no':
-//         break;
-//     default:
-//         break;
-// }
-// // function myFunction() {
-// //     var str = "Hello World!";
-// //     var res = str.toUpperCase();
-// //     document.getElementById("demo").innerHTML = res;
+// // var name = prompt('do you like coding ?');
+// // var res = name.toUpperCase();
+// // switch (name) {
+// //     case 'yes':
+// //         break;
+// //     case 'no':
+// //         break;
+// //     default:
+// //         break;
 // // }
-// function func() { 
-//     var str = 'name'; 
-//     var string = str.toUpperCase(); 
-//     document.write(string); 
-// } 
-// func(); 
-'use strict'
-var numberofcorrectanswoer = 0;
+// // var name = prompt('do you like pasta  ?');
+// // var res = name.toUpperCase();
+// // switch (name) {
+// //     case 'yes':
+// //         break;
+// //     case 'no':
+// //         break;
+// //     default:
+// //         break;
+// // }
+// // var name = prompt('do you like memes ?');
+// // var res = name.toUpperCase();
+// // switch (name) {
+// //     case 'yes':
+// //         break;
+// //     case 'no':
+// //         break;
+// //     default:
+// //         break;
+// // }
+// // var name = prompt('are you alive  ?');
+// // var res = name.toUpperCase();
+// // switch (name) {
+// //     case 'yes':
+// //         break;
+// //     case 'no':
+// //         break;
+// //     default:
+// //         break;
+// // }
+// // // function myFunction() {
+// // //     var str = "Hello World!";
+// // //     var res = str.toUpperCase();
+// // //     document.getElementById("demo").innerHTML = res;
+// // // }
+// // function func() { 
+// //     var str = 'name'; 
+// //     var string = str.toUpperCase(); 
+// //     document.write(string); 
+// // } 
+// // func(); 
+// 'use strict'
+// var numberofcorrectanswoer = 0;
 
-var userInput = prompt("what is your name?")
-alert("welcome" + userInput)
+// var userInput = prompt("what is your name?")
+// alert("welcome" + userInput)
 
-"yes" == true;
-"y" == true;
-"no" == false;
-"n" == false
+// "yes" == true;
+// "y" == true;
+// "no" == false;
+// "n" == false
 
-var q1 = prompt("Do I like to do sports?(yes/no)")
-if (q1.toLocaleLowerCase() == "yes") {
-    numberofcorrectanswoer++;
-    alert("right answer")
-    console.log("right")
-}
-else {
+// var q1 = prompt("Do I like to do sports?(yes/no)")
+// if (q1.toLocaleLowerCase() == "yes") {
+//     numberofcorrectanswoer++;
+//     alert("right answer")
+//     console.log("right")
+// }
+// else {
 
-    alert("wrong answer")
-    console.log("Wrong")
+//     alert("wrong answer")
+//     console.log("Wrong")
 
-}
-var q2 = prompt("do you like food in general (yes/no)?")
-if (q2.toLocaleLowerCase() == "yes") {
-    numberofcorrectanswoer++;
-    alert("probably fat")
-    console.log("right")
+// }
+// var q2 = prompt("do you like food in general (yes/no)?")
+// if (q2.toLocaleLowerCase() == "yes") {
+//     numberofcorrectanswoer++;
+//     alert("probably fat")
+//     console.log("right")
 
-}
+// }
 
-else {
-    alert("wrong answer")
-    console.log("wrong")
+// else {
+//     alert("wrong answer")
+//     console.log("wrong")
 
-}
+// }
 
-var q3 = prompt("do you like diving (yes/no)?")
-if (q3.toLocaleLowerCase() == "yes") {
+// var q3 = prompt("do you like diving (yes/no)?")
+// if (q3.toLocaleLowerCase() == "yes") {
 
-    alert("spongbob")
-    console.log("wrong")
+//     alert("spongbob")
+//     console.log("wrong")
 
-}
-else {
+// }
+// else {
 
-    numberofcorrectanswoer++;
-    alert("to sad ")
-    console.log("right")
+//     numberofcorrectanswoer++;
+//     alert("to sad ")
+//     console.log("right")
 
-}
-
-
-var q4 = prompt("do i will die single (yes/no)?")
-if (q4.toLocaleLowerCase() == "yes") {
-    alert("i hate life")
-    console.log("wrong")
-}
-else {
-    numberofcorrectanswoer++;
-    alert("i dont think so ")
-    console.log("right")
-
-}
-
-var q5 = prompt("do you love cars(yes/no)?")
-if (q5.toLocaleLowerCase() == "yes") {
-    numberofcorrectanswoer++;
-    alert("i gonna still die single ")
-    console.log("right")
-}
-else {
-
-    alert("wrong answer")
-    console.log("wrong")
-
-}
-
-
-
-var numberOftrial = 0;
-while (numberOftrial < 4) {
-    var q6 = parseInt(prompt("My age is :"));
-    if (q6 == 27) {
-        alert("your answer is true ");
-        numberofcorrectanswoer++;
-        break;
-    }
-    else if (q6 < 27) {
-        alert("your answer is too low ");
-        numberOftrial++;
-    }
-    else if (q6 > 27) {
-        alert("your answer is too high  ");
-        numberOftrial++;
-    }
-    if (numberOftrial == 5) {
-        alert(" OUT OF CHANCES ");
-        break;
-
-    }
-
-}
+// }
 
 
+// var q4 = prompt("do i will die single (yes/no)?")
+// if (q4.toLocaleLowerCase() == "yes") {
+//     alert("i hate life")
+//     console.log("wrong")
+// }
+// else {
+//     numberofcorrectanswoer++;
+//     alert("i dont think so ")
+//     console.log("right")
 
-var hoppies = ["gaming", "being funny", "dancing", "workout", "kickboxing"];
+// }
 
-for (var i=0; i< 6;i++){
-// var numberoftrail = 0;
-// while (numberoftrail < 6) {
-//     debugger;
-    var q7 = prompt("Guess one of my hoppies ").toLowerCase();
-    var result = true;
-    for (var j = 0; j < hoppies.length; j++) {
+// var q5 = prompt("do you love cars(yes/no)?")
+// if (q5.toLocaleLowerCase() == "yes") {
+//     numberofcorrectanswoer++;
+//     alert("i gonna still die single ")
+//     console.log("right")
+// }
+// else {
 
-        if (q7 == hoppies[j]) {
-            result = true;
-            break;
-        } else {
-            result = false;
-        }
-    }
+//     alert("wrong answer")
+//     console.log("wrong")
 
-    if (result == true) {
-        alert("you are totaly right " + q7 + "is one of my hoppies ");
-        numberofcorrectanswoer++;
-        break;
-
-    }
-    if (result) {
-        alert(q7 + "is not one of my hoppies ");
-    }
+// }
 
 
-    if (numberoftrail == 5) {
-        alert("OUT OF CHANCES ");
 
-    }
-    numberoftrail++;
+// var numberOftrial = 0;
+// while (numberOftrial < 4) {
+//     var q6 = parseInt(prompt("My age is :"));
+//     if (q6 == 27) {
+//         alert("your answer is true ");
+//         numberofcorrectanswoer++;
+//         break;
+//     }
+//     else if (q6 < 27) {
+//         alert("your answer is too low ");
+//         numberOftrial++;
+//     }
+//     else if (q6 > 27) {
+//         alert("your answer is too high  ");
+//         numberOftrial++;
+//     }
+//     if (numberOftrial == 5) {
+//         alert(" OUT OF CHANCES ");
+//         break;
 
-}
+//     }
+
+// }
+
+
+
+// var hoppies = ["gaming", "being funny", "dancing", "workout", "kickboxing"];
+
+// for (var i=0; i< 6;i++){
+// // var numberoftrail = 0;
+// // while (numberoftrail < 6) {
+// //     debugger;
+//     var q7 = prompt("Guess one of my hoppies ").toLowerCase();
+//     var result = true;
+//     for (var j = 0; j < hoppies.length; j++) {
+
+//         if (q7 == hoppies[j]) {
+//             result = true;
+//             break;
+//         } else {
+//             result = false;
+//         }
+//     }
+
+//     if (result == true) {
+//         alert("you are totaly right " + q7 + "is one of my hoppies ");
+//         numberofcorrectanswoer++;
+//         break;
+
+//     }
+//     if (result) {
+//         alert(q7 + "is not one of my hoppies ");
+//     }
+
+
+//     if (numberoftrail == 5) {
+//         alert("OUT OF CHANCES ");
+
+//     }
+//     numberoftrail++;
+
+// }
 
 // alert("the possible correct answers are[" + "singing" + "," + "reading" + "," + "dancing" + "," + "coocking" + "," + "kickboxing" + "]")
-// alert("Your result is " + numbe
+// alert("Your result is " + number) 
